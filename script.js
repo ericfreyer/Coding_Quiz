114540826380aed84372d51dd8fb029925cfc061
Eric Freyer <ericcfreyer@gmail.com>
2020-11-25 12:49:59 -0500

    commiting

--- script.js
+++ script.js
@@ -1,55 +1,64 @@
-//look at the speedreader to figure out popping up new window
-//need to write an array of questions for the div
+var Question = [
+  {
+    Title: "Commonly used datatypes DO NOT include ________.",
+    Choices: ["Strings", "Booleans", "Alerts", "Numbers"],
+    Answer: "Alerts"
+  },
+  {
+    Title: "The condition of an if / else statement is enclosed within _______.",
+    Choices: ["Quotes", "Curly Braces", "Parenthesis", "Square Brackets"],
+    Answer: "Curly Braces"
+  },
+  {
+    Title: "Arrays in javascript can be used to store _______.",
+    Choices: ["Numbers and Strings", "Other Arrays", "Booleans", "All of the Above"],
+    Answer: "All of the Above"
+  },
+  {
+    Title: "String values must be enclosed within _______ when being assigned to variables.",
+    Choices: ["Commas", "Curly Braces", "Quotes", "Parenthesis"],
+    Answer: "Commas"
+  }
+]
+
+
+
 var body = document.body;
 var score = 0
 var questionTitle = document.getElementById("Questiontitle");
 var questionContent = document.getElementById("Questioncontent");
-var answerOne = document.getElementById("Answer1")
-var answerTwo = document.getElementById("Answer2")
-var answerThree = document.getElementById("Answer3")
-var answerFour = document.getElementById("Answer4")
 var continueButton = document.getElementById("Continue");
 var quizDiv = document.getElementById("Quizdiv");
+var answerOne = document.getElementById("Answer1");
+var answerTwo = document.getElementById("Answer2");
+var answerThree = document.getElementById("Answer3");
+var answerFour = document.getElementById("Answer4");
+var answerBtns = document.getElementById("AnswerBtns");
 
-
-  questionTitle.textContent = "Coding Quiz"
-  questionContent.textContent = "Press continue to start the timer and begin the quiz."
-  continueButton.textContent = "Continue"
+  questionTitle.innerHTML = "Coding Quiz"
+  questionContent.innerHTML = "Press continue to start the timer and begin the quiz."
+  continueButton.innerHTML = "Continue"
   answerOne.style.visibility = "hidden"
   answerTwo.style.visibility = "hidden"
   answerThree.style.visibility = "hidden"
   answerFour.style.visibility = "hidden"
 
-  continueButton.addEventListener("click", questionOne)
-
-  function questionOne (){
-    continueButton.style.visibility = "hidden"
-    questionTitle.style.visibility = "hidden"
-    questionContent.textContent = "Commonly used datatypes do not include ___."
-    answerOne.style.visibility = "visible"
-    answerOne.textContent = "1. String"
-    answerTwo.style.visibility = "visible"
-    answerTwo.textContent = "2. Booleans"
-    answerThree.style.visibility = "visible"
-    answerThree.textContent = "3. Alerts"
-
-    answerFour.style.visibility = "visible"
-    answerFour.textContent = "4. Numbers"
-    answerOne.addEventListener("click", questionTwo)
-    answerTwo.addEventListener("click", questionTwo)
-    answerThree.addEventListener("click", questionTwo)
-    answerFour.addEventListener("click", questionTwo)
-   
-    
-  } 
-
-function questionTwo (){
+  
 
 
-  questionContent.textContent = "The condition of an if / else statement is enclosed within _______. "
-  answerOne.textContent = "1. Quotes"
-  answerTwo.textContent = "2. Curly Braces"
-  answerThree.textContent = "3. Parenthesis"
-  answerFour.textContent = "4. Square Brackets"
 
+  continueButton.addEventListener("click", getQuestions)
+
+function getQuestions(){
+  answerOne.style.visibility = "visible"
+  answerTwo.style.visibility = "visible"
+  answerThree.style.visibility = "visible"
+  answerFour.style.visibility = "visible"
+
+  
+questionTitle.innerHTML = Question[0].Title;
+answerOne.innerHTML = Question[0].Choices[0]
+answerTwo.innerHTML = Question[0].Choices[1]
+answerThree.innerHTML = Question[0].Choices[2]
+answerFour.innerHTML = Question[0].Choices[3]
 }
